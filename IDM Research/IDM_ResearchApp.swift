08efdd9769a058bdf524490c50579ee949d8c9fb
prashastsingh <mailme.prashast@gmail.com>
2025-04-02 14:18:09 +0200

    Initial Commit

---
+++ IDM Research/IDM_ResearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDM_ResearchApp.swift
+//  IDM Research
+//
+//  Created by Prashast Singh on 02.04.25.
+//
+
+import SwiftUI
+
+@main
+struct IDM_ResearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
